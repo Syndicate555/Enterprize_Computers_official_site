9f169bc6a6b7f3771f187bf98a23d65258faa193
Syndicate555 <saziz028@uottawa.ca>
2020-07-14 01:01:33 -0400

    changes

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,24 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const { User } = require("../models/User");
-
+const multer = require("multer");
 const { auth } = require("../middleware/auth");
+const { response } = require("express");
+
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "upload/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== ".jpg" || ext !== ".png") {
+      return cb(res.status(400).end("only jpg, png are allowed"), false);
+    }
+    cb(null, true);
+  },
+});
 
 //=================================
 //            Product
 //=================================
 
-router.get("/uploadImage", auth, (req, res) => {
-  res.status(200).json({
-    _id: req.user._id,
-    isAdmin: req.user.role === 0 ? false : true,
-    isAuth: true,
-    email: req.user.email,
-    name: req.user.name,
-    lastname: req.user.lastname,
-    role: req.user.role,
-    image: req.user.image,
-  });
+router.post("/uploadImage", auth, (req, res) => {
+  // We receive the image from the client side
+  // We Save it inside this NOde server using Multer Library
 });
 
 module.exports = router;
