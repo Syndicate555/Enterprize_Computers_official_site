575e656b3a9d3ab48ba500bdd73fd3cd1e93f26e
Syndicate555 <saziz028@uottawa.ca>
2020-07-14 00:43:04 -0400

    Update product.js

--- server/routes/product.js
+++ server/routes/product.js
@@ -5,7 +5,7 @@ const { User } = require("../models/User");
 const { auth } = require("../middleware/auth");
 
 //=================================
-//             User
+//            Product
 //=================================
 
 router.get("/auth", auth, (req, res) => {
@@ -19,54 +19,6 @@ router.get("/auth", auth, (req, res) => {
     role: req.user.role,
     image: req.user.image,
   });
-});
-
-router.post("/register", (req, res) => {
-  const user = new User(req.body);
-
-  user.save((err, doc) => {
-    if (err) return res.json({ success: false, err });
-    return res.status(200).json({
-      success: true,
-    });
-  });
-});
-
-router.post("/login", (req, res) => {
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (!user)
-      return res.json({
-        loginSuccess: false,
-        message: "Auth failed, email not found",
-      });
-
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch)
-        return res.json({ loginSuccess: false, message: "Wrong password" });
-
-      user.generateToken((err, user) => {
-        if (err) return res.status(400).send(err);
-        res.cookie("w_authExp", user.tokenExp);
-        res.cookie("w_auth", user.token).status(200).json({
-          loginSuccess: true,
-          userId: user._id,
-        });
-      });
-    });
-  });
-});
-
-router.get("/logout", auth, (req, res) => {
-  User.findOneAndUpdate(
-    { _id: req.user._id },
-    { token: "", tokenExp: "" },
-    (err, doc) => {
-      if (err) return res.json({ success: false, err });
-      return res.status(200).send({
-        success: true,
-      });
-    }
-  );
 });
 
 module.exports = router;
