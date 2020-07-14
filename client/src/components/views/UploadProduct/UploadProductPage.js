0d75050bc409664b4ceb7093b60603f2ce6e76f1
Syndicate555 <saziz028@uottawa.ca>
2020-07-13 22:21:38 -0400

    Update UploadProductPage.js

--- client/src/components/views/UploadProduct/UploadProductPage.js
+++ client/src/components/views/UploadProduct/UploadProductPage.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 function UploadProductPage() {
+  const [TitleValue, setTitleValue] = useState("");
+  const [DescriptionValue, setDescriptionValue] = useState("");
+  const [PriceValue, setPriceValue] = useState("");
+  const onTitleChange = (event) => {
+    setTitleValue(event.currentTarget.value);
+  };
+  const onDescriptionChange = (event) => {
+    setDescriptionValue(event.currentTarget.value);
+  };
+  const onPriceChange = (event) => {
+    setPriceValue(event.currentTarget.value);
+  };
   return (
     <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
       <div style={{ textAlign: "center", marginBottom: "2rem" }}>
         <h2> Upload Travel Product</h2>
       </div>
 
-      <form>
+      <form onSubmit>
         {/* DropZone */}
         <br></br>
         <br></br>
         <label> Title </label>
-        <input />
+        <input onChange={onTitleChange} value={TitleValue} />
         <br></br>
         <br></br>
         <label>Description</label>
-        <textarea />
+        <br></br>
+        <textarea onChange={onDescriptionChange} value={DescriptionValue} />
         <br></br>
         <br></br>
         <label>Price($)</label>
+        <br />
+        <br />
+        <input onChange={setPriceValue} value={PriceValue} />
+        <select>
+          <option></option>
+        </select>
+        <br></br>
+        <br></br>
+        <button>Submit</button>
       </form>
     </div>
   );
