cf90489771808de5a73fe911b79407d47963aad4
Syndicate555 <saziz028@uottawa.ca>
2020-07-15 06:50:25 -0400

    chnages

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
 import Axios from "axios";
+import ImageSlider from "../../utils/ImageSlider";
+
 import { Icon, Col, Card, Row } from "antd";
 const { Meta } = Card;
 
@@ -19,7 +21,7 @@ function LandingPage() {
   const renderCards = Products.map((product, index) => {
     return (
       <Col lg={6} md={8} xs={24}>
-        <Card hoverable={true} cover>
+        <Card hoverable={true} cover={<ImageSlider images={product.images} />}>
           <Meta title={product.title} description={`$${product.price}`} />
         </Card>
       </Col>
