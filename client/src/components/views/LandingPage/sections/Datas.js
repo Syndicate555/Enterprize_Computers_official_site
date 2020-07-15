fff3b62c8dc71996502808d3c7db5172e969411a
Syndicate555 <saziz028@uottawa.ca>
2020-07-15 09:58:42 -0400

    changes

---
+++ client/src/components/views/LandingPage/sections/Datas.js
@@ -0,0 +1,83 @@
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $199",
+        "array": [0, 199]
+    },
+    {
+        "_id": 2,
+        "name": "$200 to $249",
+        "array": [200, 249]
+    },
+    {
+        "_id": 3,
+        "name": "$250 to $279",
+        "array": [250, 279]
+    },
+    {
+        "_id": 4,
+        "name": "$280 to $299",
+        "array": [280, 299]
+    },
+    {
+        "_id": 5,
+        "name": "More than $300",
+        "array": [300, 1500000]
+    }
+]
+
+
+
+const categories = [
+    {
+        "_id": 1,
+        "name": "Desktop PCs and Software"
+    },
+    {
+        "_id": 2,
+        "name": "Notebooks and Tablets"
+    },
+    {
+        "_id": 3,
+        "name": "Case & Cooling"
+    },
+    {
+        "_id": 4,
+        "name": "Computer Components"
+    },
+    {
+        "_id": 5,
+        "name": "CPUs"
+    },
+    {
+        "_id": 6,
+        "name": "Flash/Storage Media"
+    },
+    {
+        "_id": 7,
+        "name": "Smart Phones & Gadgets"
+    },
+    {
+        "_id": 8,
+        "name": "Hard Drives & SSDs"
+    }
+    {
+        "_id": 9,
+        "name": "Headphones & Speakers"
+    },
+    {
+        "_id": 10,
+        "name": "Peripherals"
+    },
+]
+
+
+export {
+    price,
+    categories
+}
