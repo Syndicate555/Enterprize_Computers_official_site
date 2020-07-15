32fbddd21d62d00959cb60577c03cc77e9e83baf
Syndicate555 <saziz028@uottawa.ca>
2020-07-15 06:34:47 -0400

    Update LandingPage.js

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
 import Axios from "axios";
+import { Icon, Col, Card, Row } from "antd";
 
 function LandingPage() {
   const [Products, setProducts] = useState([]);
@@ -16,7 +17,11 @@ function LandingPage() {
   return (
     <div style={{ width: "75%", margin: "3rem auto" }}>
       <div style={{ textAlign: "center" }}>
-        <h2> Welcome to the World of Computers </h2>
+        <h2>
+          {" "}
+          Welcome to the World of Computers
+          <Icon type="rocket" />{" "}
+        </h2>
       </div>
     </div>
   );
