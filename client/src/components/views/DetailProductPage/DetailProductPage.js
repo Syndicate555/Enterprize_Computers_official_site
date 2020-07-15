ae15b911247c85c602fea25524376ec67c2045b1
Syndicate555 <saziz028@uottawa.ca>
2020-07-15 18:33:58 -0400

    changes

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import { Row, Col } from "antd";
-
+import { addToCart } from "../../../_actions/user_actions";
 import ProductImage from "./Sections/ProductImage";
 import ProductInfo from "./Sections/ProductInfo";
+import { useDispatch } from "react-redux";
+
 function DetailProductPage(props) {
+  const dispatch = useDispatch();
+
   const productId = props.match.params.productId;
   const [Product, setProduct] = useState([]);
 
@@ -15,6 +19,9 @@ function DetailProductPage(props) {
       }
     );
   }, []);
+  const addToCartHandler = (productId) => {
+    dispatch(addToCart(productId));
+  };
   return (
     <div className="postPage" style={{ width: "100%", padding: "3rem 4rem" }}>
       <div style={{ display: "flex", justifyContent: "center" }}>
@@ -28,7 +35,7 @@ function DetailProductPage(props) {
           <ProductImage detail={Product} />
         </Col>
         <Col lg={12} xs={24}>
-          <ProductInfo addToCart detail={Product} />
+          <ProductInfo addToCart={addToCartHandler} detail={Product} />
         </Col>
       </Row>
     </div>
